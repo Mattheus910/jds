4650b057a4665e710e897cfb7686147829efb9b9
Mattheus910 <123586175+Mattheus910@users.noreply.github.com>
2023-11-21 15:53:01 -0300

    ajuste no javascript

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 const hamburguer = document.querySelector("#menu picture #hamburguer");
 const nav = document.querySelector("#menu #nav");
 
-hamburguer.addEventListener("click", function(){ 
-   if (nav.style.display == "none"){
-    nav.style.display = "flex";
+hamburguer.addEventListener("click", function () {
+   const navDisplay = window.getComputedStyle(nav).getPropertyValue("display");
+
+   if (navDisplay === "none") {
+      nav.style.display = "flex";
    } else {
-    nav.style.display = "none";
+      nav.style.display = "none";
    }
 });
